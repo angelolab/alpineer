11cbb739a87f1eb487b84eab79bd4d5b0f7d18b8
Sricharan Reddy Varra <srivarra@stanford.edu>
2022-11-15 15:23:59 -0800

    Added Utility Functions (#13)
    * added all utility functions
    
    * added _make_small_file from toffy
    
    * added data_utils.stitch_images
    
    * added settings.py for file extension types
    
    * removed unused functions

--- src/tmi/misc_utils.py
+++ src/tmi/misc_utils.py
@@ -1,25 +1,29 @@
 import os
-from typing import Any, List, Optional
+import warnings
+from collections.abc import Iterable
+from typing import Any
 
 import matplotlib.pyplot as plt
 import numpy as np
+import xarray as xr
+
+from tmi import io_utils
 
 
-def save_figure(save_dir, save_file: str, dpi: Optional[float] = 300) -> None:
-    """Saves the figure in a directory.
+def save_figure(save_dir, save_file, dpi=None):
+    """Verify save_dir and save_file, then save to specified location
 
     Args:
-        save_dir (str): the name of the directory we wish to save to.
-        save_file (str): The file we wish to save to
-        dpi (float, optional): The pixel density of the figure. Defaults to 300.
-
-    Raises:
-        FileNotFoundError: The save directory does not exist.
-        FileNotFoundError: The save file specified does not exist.
+        save_dir (str):
+            the name of the directory we wish to save to
+        save_file (str):
+            the name of the file we wish to save to
+        dpi (float):
+            the resolution of the figure
     """
-    # verify save_dir exists
-    if not os.path.exists(save_dir):
-        raise FileNotFoundError("save_dir %s does not exist" % save_dir)
+
+    # path validation
+    io_utils.validate_paths(save_dir)
 
     # verify that if save_dir specified, save_file must also be specified
     if save_file is None:
@@ -28,7 +32,7 @@ def save_figure(save_dir, save_file: str, dpi: Optional[float] = 300) -> None:
     plt.savefig(os.path.join(save_dir, save_file), dpi=dpi)
 
 
-def create_invalid_data_str(invalid_data: List[str]) -> str:
+def create_invalid_data_str(invalid_data):
     """Creates a easy to read string for ValueError statements.
 
     Args:
@@ -54,22 +58,55 @@ def create_invalid_data_str(invalid_data: List[str]) -> str:
     return err_str_data
 
 
-def verify_in_list(**kwargs: Any) -> None:
-    """Verify at least whether the values in the first list exist in the second.
+def make_iterable(a: Any, ignore_str: bool = True):
+    """Convert noniterable type to singleton in list
 
     Args:
+        a (T | Iterable[T]):
+            value or iterable of type T
+        ignore_str (bool):
+            whether to ignore the iterability of the str type
+
+    Returns:
+        List[T]:
+            a as singleton in list, or a if a was already iterable.
+    """
+    return (
+        a
+        if isinstance(a, Iterable)
+        and not ((isinstance(a, str) and ignore_str) or isinstance(a, type))
+        else [a]
+    )
+
+
+def verify_in_list(warn=False, **kwargs):
+    """Verify at least whether the values in the first list exist in the second
+
+    Args:
+        warn (bool):
+            Whether to issue warning instead of error, defaults to False
         **kwargs (list, list):
             Two lists, but will work for single elements as well.
             The first list specified will be tested to see
             if all its elements are contained in the second.
 
     Raises:
-        ValueError: if not all values in the first list are found in the second
+        ValueError:
+            if not all values in the first list are found in the second
+        Warning:
+            if not all values are found and warn is True
     """
+
     if len(kwargs) != 2:
         raise ValueError("You must provide 2 arguments to verify_in_list")
 
     test_list, good_values = kwargs.values()
+    test_list = list(make_iterable(test_list))
+    good_values = list(make_iterable(good_values))
+
+    for v in [test_list, good_values]:
+        if len(v) == 0:
+            raise ValueError("List arguments cannot be empty")
 
     if not np.isin(test_list, good_values).all():
         test_list_name, good_values_name = kwargs.keys()
@@ -80,43 +117,57 @@ def verify_in_list(**kwargs: Any) -> None:
         difference = [str(val) for val in test_list if val not in good_values]
 
         # Only printing up to the first 10 invalid values.
-        err_str = ("Displaying {} of {} invalid value(s) provided for list {:^}.\n").format(
-            min(len(difference), 10), len(difference), test_list_name
+        err_str = (
+            "Not all values given in list {0:^} were found in list {1:^}.\n "
+            "Displaying {2} of {3} invalid value(s) for list {4:^}\n"
+        ).format(
+            test_list_name,
+            good_values_name,
+            min(len(difference), 10),
+            len(difference),
+            test_list_name,
         )
 
         err_str += create_invalid_data_str(difference)
 
-        raise ValueError(err_str)
+        if warn:
+            warnings.warn(err_str)
+        else:
+            raise ValueError(err_str)
 
-    print(f"All values in list {test_list_name} exist in list {good_values_name}")
-    return
 
-
-def verify_same_elements(**kwargs: Any) -> None:
+def verify_same_elements(enforce_order=False, warn=False, **kwargs):
     """Verify if two lists contain the same elements regardless of count
 
     Args:
-        **kwargs (list, list): Two lists
+        enforce_order (bool):
+            Whether to also check for the same ordering between the two lists
+        warn (bool):
+            Whether to issue warning instead of error, defaults to False
+        **kwargs (list, list):
+            Two lists
 
     Raises:
-        ValueError: if the two lists don't contain the same elements
+        ValueError:
+            if the two lists don't contain the same elements
     """
+
     if len(kwargs) != 2:
-        raise ValueError("You must provide 2 arguments to verify_same_elements")
+        raise ValueError("You must provide 2 list arguments to verify_same_elements")
 
     list_one, list_two = kwargs.values()
 
     try:
-        list_one_cast: List[Any] = list(list_one)
-        list_two_cast: List[Any] = list(list_two)
-    except TypeError as e:
-        raise ValueError("Both arguments provided must be lists or list types") from e
+        list_one_cast = list(list_one)
+        list_two_cast = list(list_two)
+    except TypeError:
+        raise ValueError("Both arguments provided must be lists or list types")
+
+    list_one_name, list_two_name = kwargs.keys()
+    list_one_name = list_one_name.replace("_", " ")
+    list_two_name = list_two_name.replace("_", " ")
 
     if not np.all(set(list_one_cast) == set(list_two_cast)):
-        list_one_name, list_two_name = kwargs.keys()
-        list_one_name = list_one_name.replace("_", " ")
-        list_two_name = list_two_name.replace("_", " ")
-
         # Values in list one that are not in list two
         missing_vals_1 = [str(val) for val in (set(list_one_cast) - set(list_two_cast))]
 
@@ -127,26 +178,55 @@ def verify_same_elements(**kwargs: Any) -> None:
         missing_vals_total = [str(val) for val in set(list_one_cast) ^ set(list_two_cast)]
 
         err_str = (
-            "{} value(s) provided for list {:^} and list {:^} are not found in both lists.\n"
+            "{0} value(s) provided for list {1:^} and list {2:^} are not found in both lists.\n"
         ).format(len(missing_vals_total), list_one_name, list_two_name)
 
         # Only printing up to the first 10 invalid values for list one.
-        err_str += ("{:>13} \n").format(
-            "Displaying {} of {} missing value(s) for list {}\n".format(
-                min(len(missing_vals_1), 10), len(missing_vals_2), list_one_name
+        err_str += ("{0:>13} \n").format(
+            "Displaying {0} of {1} value(s) in list {2} that are missing from list {3}\n".format(
+                min(len(missing_vals_1), 10), len(missing_vals_1), list_one_name, list_two_name
             )
         )
         err_str += create_invalid_data_str(missing_vals_1) + "\n"
 
         # Only printing up to the first 10 invalid values for list two
-        err_str += ("{:>13} \n").format(
-            "Displaying {} of {} missing value(s) for list {}\n".format(
-                min(len(missing_vals_2), 10), len(missing_vals_2), list_two_name
+        err_str += ("{0:>13} \n").format(
+            "Displaying {0} of {1} value(s) in list {2} that are missing from list {3}\n".format(
+                min(len(missing_vals_2), 10), len(missing_vals_2), list_two_name, list_one_name
             )
         )
         err_str += create_invalid_data_str(missing_vals_2) + "\n"
 
-        raise ValueError(err_str)
+        if warn:
+            warnings.warn(err_str)
+        else:
+            raise ValueError(err_str)
+    elif enforce_order and list_one_cast != list_two_cast:
+        first_bad_index = next(
+            i for i, (l1, l2) in enumerate(zip(list_one_cast, list_two_cast)) if l1 != l2
+        )
 
-    print(f"List {list_one_name} and list {list_two_name} contain the same elements")
-    return
+        err_str = "Lists %s and %s ordered differently: values %s and %s do not match at index %d"
+
+        if warn:
+            warnings.warn(
+                err_str
+                % (
+                    list_one_name,
+                    list_two_name,
+                    list_one_cast[first_bad_index],
+                    list_two_cast[first_bad_index],
+                    first_bad_index,
+                )
+            )
+        else:
+            raise ValueError(
+                err_str
+                % (
+                    list_one_name,
+                    list_two_name,
+                    list_one_cast[first_bad_index],
+                    list_two_cast[first_bad_index],
+                    first_bad_index,
+                )
+            )
--- tests/misc_utils_test.py
+++ tests/misc_utils_test.py
@@ -1,64 +1,133 @@
+import os
+import tempfile
+
+import numpy as np
 import pytest
 
-from tmi.misc_utils import create_invalid_data_str, verify_in_list, verify_same_elements
+from tmi import misc_utils
 
 
-def test_verify_in_list() -> None:
-    """Tests that the function `misc_utils::verify_in_list`
-    behaves properly.
-    """
+def test_verify_in_list():
     with pytest.raises(ValueError):
         # not passing two lists to verify_in_list
-        verify_in_list(one=["not_enough"])
+        misc_utils.verify_in_list(one=["not_enough"])
 
     with pytest.raises(ValueError):
         # value is not contained in a list of acceptable values
-        verify_in_list(one="hello", two=["goodbye", "hello world"])
+        misc_utils.verify_in_list(one="hello", two=["goodbye", "hello world"])
 
     with pytest.raises(ValueError):
         # not every element in a list is equal to an value
-        verify_in_list(one=["goodbye", "goodbye", "hello"], two="goodbye")
+        misc_utils.verify_in_list(one=["goodbye", "goodbye", "hello"], two="goodbye")
 
     with pytest.raises(ValueError):
         # one list is not completely contained in another
-        verify_in_list(one=["hello", "world"], two=["hello", "goodbye"])
+        misc_utils.verify_in_list(one=["hello", "world"], two=["hello", "goodbye"])
+
+    with pytest.warns(UserWarning):
+        # issues a warning instead of raising an error
+        misc_utils.verify_in_list(warn=True, one=["hello", "world"], two=["hello", "goodbye"])
+
+    # test unwrapped string
+    misc_utils.verify_in_list(one="hello", two=["hello", "world"])
+
+    # test numpy array allowance
+    misc_utils.verify_in_list(
+        one=np.array(["hello", "world"]), two=np.array(["hello", "world", "!"])
+    )
 
 
-def test_verify_same_elements() -> None:
-    """Tests that the function `misc_utils::verify_same_elements`
-    behaves properly.
-    """
+def test_verify_same_elements():
     with pytest.raises(ValueError):
         # not passing two lists to verify_same_elements
-        verify_same_elements(one=["not_enough"])
+        misc_utils.verify_same_elements(one=["not_enough"])
 
     with pytest.raises(ValueError):
         # not passing in items that can be cast to list for either one or two
-        verify_same_elements(one=1, two=2)
+        misc_utils.verify_same_elements(one=1, two=2)
 
     with pytest.raises(ValueError):
         # the two lists provided do not contain the same elements
-        verify_same_elements(one=["elem1", "elem2", "elem2"], two=["elem2", "elem2", "elem4"])
+        misc_utils.verify_same_elements(
+            one=["elem1", "elem2", "elem2"], two=["elem2", "elem2", "elem4"]
+        )
+
+    with pytest.raises(ValueError):
+        # the two lists provided differ in length (ordered checking)
+        misc_utils.verify_same_elements(enforce_order=True, one=["elem1"], two=["elem1", "elem2"])
+
+    with pytest.raises(ValueError):
+        # the two lists are ordered differently
+        misc_utils.verify_same_elements(
+            enforce_order=True, one=["elem1", "elem2"], two=["elem2", "elem1"]
+        )
+
+    with pytest.warns(UserWarning):
+        # issues a warning instead of raising an error
+        misc_utils.verify_same_elements(
+            warn=True, one=["elem1", "elem2", "elem2"], two=["elem2", "elem2", "elem4"]
+        )
+
+    with pytest.warns(UserWarning):
+        # issues a warning instead of raising an error for ordered
+        misc_utils.verify_same_elements(
+            enforce_order=True, warn=True, one=["elem1", "elem2"], two=["elem2", "elem1"]
+        )
 
 
-def test_create_invalid_data_str() -> None:
-    """Tests that the function `misc_utils::create_invalid_data_str`
-    behaves properly.
-    """
+def test_create_invalid_data_str():
     invalid_data = ["data_" + str(i) for i in range(20)]
 
     # Test to make sure the case of 10 invalid values creates a proper string.
-    invalid_data_str1 = create_invalid_data_str(invalid_data=invalid_data[:10])
-    for data in invalid_data[:10]:
-        assert invalid_data_str1.find(data) != -1
+    invalid_data_str1 = misc_utils.create_invalid_data_str(invalid_data=invalid_data[:10])
+    for id in invalid_data[:10]:
+        assert invalid_data_str1.find(id) != -1
 
     # Test to make sure cases of less than 10 invalid values creates a proper string.
-    invalid_data_str2 = create_invalid_data_str(invalid_data=invalid_data[:3])
-    for data in invalid_data[:3]:
-        assert invalid_data_str2.find(data) != -1
+    invalid_data_str2 = misc_utils.create_invalid_data_str(invalid_data=invalid_data[:3])
+    for id in invalid_data[:3]:
+        assert invalid_data_str2.find(id) != -1
 
     # Test to make sure cases of more than 10 invalid values creates a proper string
     # capping out at 10 values.
-    invalid_data_str3 = create_invalid_data_str(invalid_data=invalid_data)
-    for data in invalid_data[:10]:
-        assert invalid_data_str3.find(data) != -1
+    invalid_data_str3 = misc_utils.create_invalid_data_str(invalid_data=invalid_data)
+    for id in invalid_data[:10]:
+        assert invalid_data_str3.find(id) != -1
+
+
+def test_make_iterable():
+    # Input is string, ignore_str = True
+    input_str = "test"
+    assert misc_utils.make_iterable(input_str, ignore_str=True) == ["test"]
+
+    # Input is string, ignore_str = False
+    assert misc_utils.make_iterable(input_str, ignore_str=False) == "test"
+
+    # Input is a list of objects, ignore_str = True
+    input_list = [1, 2, 3, 4]
+    assert misc_utils.make_iterable(input_list, ignore_str=True) == [1, 2, 3, 4]
+
+    # Input is a list of objects, ignore_str = False
+    assert misc_utils.make_iterable(input_list, ignore_str=False) == [1, 2, 3, 4]
+
+    # Input is of type(<...>) = type (a fundamental data type such as str, int, bool)
+    assert misc_utils.make_iterable(str, ignore_str=True) == [str]
+
+    assert misc_utils.make_iterable(str, ignore_str=False) == [str]
+
+    assert misc_utils.make_iterable(int, ignore_str=True) == [int]
+
+    assert misc_utils.make_iterable(int, ignore_str=False) == [int]
+
+    assert misc_utils.make_iterable(bool, ignore_str=True) == [bool]
+
+    assert misc_utils.make_iterable(bool, ignore_str=False) == [bool]
+
+    # Input is a numpy array
+    input_np_arr = np.array([1, 2, 3, 4, 5])
+    np.testing.assert_equal(
+        misc_utils.make_iterable(input_np_arr, ignore_str=True), np.array([1, 2, 3, 4, 5])
+    )
+    np.testing.assert_equal(
+        misc_utils.make_iterable(input_np_arr, ignore_str=False), np.array([1, 2, 3, 4, 5])
+    )
